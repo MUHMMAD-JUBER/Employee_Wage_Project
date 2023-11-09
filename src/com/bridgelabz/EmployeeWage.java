11540dc93d85cec5e254e97cbcfbed4bea472935
Zubair <zubairyaqoob6988@gmail.com>
2023-11-09 17:50:45 +0530

    Added : calculatin wages till number of working days or total working hours per month is reached

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,22 +6,27 @@ public class EmployeeWage {
         final int IS_PART_TIME = 1;
         final int IS_FULL_TIME = 2;
         final int EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0;
-        int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        switch((int)empCheck){
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
-        }
+        final int NUM_OF_WORKING_DAYS = 2;
+        //variables
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        //computaion
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage = empWage + 1;
             System.out.println("Emp Wage :" + empWage);
-
+        }
+        System.out.println("Total EmployeeWage :" + totalEmpWage);
     }
 }
